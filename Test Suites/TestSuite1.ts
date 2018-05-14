1b600f43ce7c5970f83f29a894d27dbfa3385b25
ionut.ciobanu <ionut.ciobanu@LENOVO0187.corp.gaminginnovationgroup.com>
2018-05-14 11:55:25 +0200

    test

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-05-14T11:26:12</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>7392c807-eaa2-4468-9278-8b7d52823704</testSuiteGuid>
+   <testCaseLink>
+      <guid>01b32c6a-f218-46db-8239-4ccf45e6f9d9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test 1 by Tom</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
