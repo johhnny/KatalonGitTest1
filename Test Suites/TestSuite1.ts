973011d30a9b78cfa5ee69c8dc64ba52a1af93d5
Ionut Ciobanu <ionut.ciobanu@corp.gaminginnovationgroup.com>
2018-05-14 12:21:01 +0200

    test message

--- Test Suites/TestSuite1.ts
+++ Test Suites/TestSuite1.ts
@@ -4,19 +4,13 @@
    <name>TestSuite1</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-05-14T12:18:08</lastRun>
+   <lastRun>2018-05-14T12:20:41</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>7392c807-eaa2-4468-9278-8b7d52823704</testSuiteGuid>
-   <testCaseLink>
-      <guid>01b32c6a-f218-46db-8239-4ccf45e6f9d9</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Test 1 by Tom</testCaseId>
-   </testCaseLink>
    <testCaseLink>
       <guid>71c86cf0-b30f-488a-87b0-ce74fd6905b2</guid>
       <isReuseDriver>false</isReuseDriver>
